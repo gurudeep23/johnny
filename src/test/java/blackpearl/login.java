a499f02ae8014e8e3fea2870e92726f33546fbd7
guru <guru.gama23@gmail.com>
2022-01-07 07:57:56 +0000

    from gibs

---
+++ src/test/java/blackpearl/login.java
@@ -0,0 +1,20 @@
+package blackpearl;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class login 
+ 
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
